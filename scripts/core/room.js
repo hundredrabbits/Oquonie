bf54c87e08b07ef9b82ddb262867417762e457ef
Devine Lu Linvega <aliceffekt@gmail.com>
2017-02-13 08:58:17 -0700

    Display room

--- scripts/core/room.js
+++ scripts/core/room.js
@@ -16,9 +16,17 @@ function Room()
 
   this.show = function()
   {
+
     for (var i = 0; i < this.floors.length; i++){
-      var tile = new Tile(1,1,this.floors[i]);
+      var pos = this.position_from_id(i);
+      var tile = new Tile(pos[0],pos[1],this.floors[i],"floor");
       this.element.appendChild(tile.element);
     }
+  }
+
+  this.position_from_id = function(id)
+  {
+    var t = [[1,-1],[1,0],[1,1],[0,-1],[0,0],[0,1],[-1,-1],[-1,0],[-1,1]]
+    return t[id];
   }
 }
--- scripts/core/tile.js
+++ scripts/core/tile.js
@@ -1,4 +1,26 @@
-function Tile()
+function Tile(x,y,id,type)
 {
   this.element = document.createElement("tile");
+  this.element.className = type;
+  this.element.setAttribute("pos",x+","+y);
+
+  var bg = "url(media/graphics/tile."+id+".png); ";
+
+  var top = "0px";
+  var left = "0px";
+  var zIndex = 100;
+
+  if(x == 1 && y == 1){ top = "40%"; left = "50%"; zIndex += 10; }
+  if(x == 1 && y == 0){ top = "45%"; left = "40%"; zIndex += 20; }
+  if(x == 1 && y == -1){ top = "50%"; left = "30%"; zIndex += 30; }
+
+  if(x == 0 && y == 1){ top = "45%"; left = "60%"; zIndex += 20; }
+  if(x == 0 && y == 0){ top = "50%"; left = "50%"; zIndex += 30; }
+  if(x == 0 && y == -1){ top = "55%"; left = "40%"; zIndex += 40; }
+
+  if(x == -1 && y == 1){ top = "50%"; left = "70%"; zIndex += 30; }
+  if(x == -1 && y == 0){ top = "55%"; left = "60%"; zIndex += 40; }
+  if(x == -1 && y == -1){ top = "60%"; left = "50%"; zIndex += 50; }
+
+  this.element.setAttribute("style","background-image:"+bg+"; left:"+left+"; top:"+top+";z-index:"+zIndex);
 }
