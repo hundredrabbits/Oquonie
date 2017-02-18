8432a24def157c4174c43633384731960c10dd37
Devine Lu Linvega <aliceffekt@gmail.com>
2017-02-18 11:51:51 -0700

    Renamed blockers

--- scripts/event/blocker.js
+++ scripts/event/blocker.js
@@ -10,7 +10,7 @@ function Blocker(x,y,id)
     return true;
   }
 
-  var bg = "url(media/graphics/blocker."+id+".png)";
+  var bg = "url(media/graphics/blocker/"+id+".png)";
 
   var p = this.position_at(x,y,100);
   var top = p[0];
