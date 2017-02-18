689b4b4a80ee045f52b0b60efdac4bbeae17fd5f
Devine Lu Linvega <aliceffekt@gmail.com>
2017-02-18 10:59:14 -0700

    Display wizards properly

--- scripts/event/wizard.js
+++ scripts/event/wizard.js
@@ -1,16 +1,17 @@
-function Wizard(x,y)
+function Wizard(x,y,character)
 {
   Event.call(this,"wizard");
 
   this.x = x;
   this.y = y;
+  this.character = character;
 
   this.is_collider = function()
   {
     return true;
   }
 
-  var bg = "url(media/graphics/event.noface.1.png)";
+  var bg = "url(media/graphics/event."+this.character+".1.png)";
 
   var p = this.position_at(x,y,100);
   var top = p[0];
