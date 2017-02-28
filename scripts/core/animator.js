c7fc643f76bf1eedfe8d06b78d5f44323a89f9b5
Devine Lu Linvega <aliceffekt@gmail.com>
2017-02-27 20:08:05 -0700

    Migrated to sprites

--- scripts/core/animator.js
+++ scripts/core/animator.js
@@ -16,7 +16,21 @@ function Animator(host)
     if(!this.animations[this.state]){ return; }
 
     var anim = this.animations[this.state];
+    var width = $(this.host.element).width();
+    var height = $(this.host.element).height();
+    var frames = unique(this.animations[this.state].frames).length;
 
-    $(this.host.element).css('background-image', "url(media/graphics/"+this.host.name+"/"+(this.host.id ? this.host.id+"." : "")+this.state+"."+anim.run()+".png)");
+    $(this.host.element).css('background-image', "url(media/graphics/"+this.host.name+"/"+(this.host.id ? this.host.id+"." : "")+this.state+".png)");
+    $(this.host.element).css('background-size',(width*frames)+"px "+(width*1.5)+"px");
+    $(this.host.element).css('background-position',(anim.run() * -width + width)+"px center");
   }
+}
+
+function unique(list)
+{
+  var result = [];
+  $.each(list, function(i, e) {
+      if ($.inArray(e, result) == -1) result.push(e);
+  });
+  return result;
 }
