ee59e089a67b6a8ab57f91cbda02ec1dae37fb08
Rezmason <jeremysachs@rezmason.net>
2017-06-10 22:20:34 -0700

    Petunia doesn't need to assign an asset to tiles that don't exist.

--- scripts/event/petunia.js
+++ scripts/event/petunia.js
@@ -21,7 +21,7 @@ function Petunia(x,y)
     oquonie.artbook.set_art("#wall_3","media/graphics/wall/"+parseInt(Math.random() * 40)+".png");
     oquonie.artbook.set_art("#wall_4","media/graphics/wall/"+parseInt(Math.random() * 40)+".png");
     oquonie.artbook.set_art("#wall_5","media/graphics/wall/"+parseInt(Math.random() * 40)+".png");
-    oquonie.artbook.set_art("#wall_6","media/graphics/wall/"+parseInt(Math.random() * 40)+".png");
+    // oquonie.artbook.set_art("#wall_6","media/graphics/wall/"+parseInt(Math.random() * 40)+".png");
     
     oquonie.artbook.set_art("#floor_1","media/graphics/floor/"+parseInt(Math.random() * 40)+".png");
     oquonie.artbook.set_art("#floor_2","media/graphics/floor/"+parseInt(Math.random() * 40)+".png");
@@ -31,7 +31,7 @@ function Petunia(x,y)
     oquonie.artbook.set_art("#floor_6","media/graphics/floor/"+parseInt(Math.random() * 40)+".png");
     oquonie.artbook.set_art("#floor_7","media/graphics/floor/"+parseInt(Math.random() * 40)+".png");
     oquonie.artbook.set_art("#floor_8","media/graphics/floor/"+parseInt(Math.random() * 40)+".png");
-    oquonie.artbook.set_art("#floor_9","media/graphics/floor/"+parseInt(Math.random() * 40)+".png");
+    // oquonie.artbook.set_art("#floor_9","media/graphics/floor/"+parseInt(Math.random() * 40)+".png");
   }
 
   this.update(20);
