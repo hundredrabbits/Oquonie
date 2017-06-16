9f6f53177dc2abf558fc2b1a9afcd55d9d1783b2
Rezmason <jeremysachs@rezmason.net>
2017-06-16 06:11:11 -0600

    Moving Hiversaires assets out of Petunia's repertoire. Replacing an unused placeholder asset with a different, unused final asset. Renaming some Hiversaires-related Camilare. Adding an idle animation and cloning Sauvegarde's dial-ogg from noface. Added starry grey background for Hiversaries.

--- scripts/event/petunia.js
+++ scripts/event/petunia.js
@@ -16,12 +16,12 @@ function Petunia(x,y)
   {
     oquonie.dialog.show("petunia",["confusion1","confusion3","confusion2"]);
 
-    oquonie.artbook.set_art("#wall_1","media/graphics/wall/"+parseInt(Math.random() * 40)+".png");
-    oquonie.artbook.set_art("#wall_2","media/graphics/wall/"+parseInt(Math.random() * 40)+".png");
-    oquonie.artbook.set_art("#wall_3","media/graphics/wall/"+parseInt(Math.random() * 40)+".png");
-    oquonie.artbook.set_art("#wall_4","media/graphics/wall/"+parseInt(Math.random() * 40)+".png");
-    oquonie.artbook.set_art("#wall_5","media/graphics/wall/"+parseInt(Math.random() * 40)+".png");
-    // oquonie.artbook.set_art("#wall_6","media/graphics/wall/"+parseInt(Math.random() * 40)+".png");
+    oquonie.artbook.set_art("#wall_1","media/graphics/wall/"+parseInt(Math.random() * 37)+".png");
+    oquonie.artbook.set_art("#wall_2","media/graphics/wall/"+parseInt(Math.random() * 37)+".png");
+    oquonie.artbook.set_art("#wall_3","media/graphics/wall/"+parseInt(Math.random() * 37)+".png");
+    oquonie.artbook.set_art("#wall_4","media/graphics/wall/"+parseInt(Math.random() * 37)+".png");
+    oquonie.artbook.set_art("#wall_5","media/graphics/wall/"+parseInt(Math.random() * 37)+".png");
+    // oquonie.artbook.set_art("#wall_6","media/graphics/wall/"+parseInt(Math.random() * 37)+".png");
     
     oquonie.artbook.set_art("#floor_1","media/graphics/floor/"+parseInt(Math.random() * 40)+".png");
     oquonie.artbook.set_art("#floor_2","media/graphics/floor/"+parseInt(Math.random() * 40)+".png");
